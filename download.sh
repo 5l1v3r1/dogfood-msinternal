2e788965d7c210d6947ec37b389c0e19bdacd34d
Ronit Sajeey <rsajeey@hotmail.com>
2018-01-13 23:14:55 +0000

    Fixes the program

--- download.sh
+++ download.sh
@@ -10,21 +10,21 @@ wget $URL/wsus/setup.exe
 
 # Creates the 'Office/Data' directory, enters the 'Office/Data' directory & downloads all of the required files
 mkdir Data && cd Data
-wget $URL/$ID/Data/v64.cab
-wget $URL/$ID/Data/v64_$VERSION.cab
+wget $URL/$ID/Office/Data/v64.cab
+wget $URL/$ID/Office/Data/v64_$VERSION.cab
 
 # Creates the 'Office/Data/$VERSION' directory, enters the 'Office/Data/$VERSION' directory & downloads all of the required files
 mkdir $VERSION && cd $VERSION
-wget $URL/$ID/Data/$VERSION/1320.cab
-wget $URL/$ID/Data/$VERSION/1640.cab
-wget $URL/$ID/Data/$VERSION/i321033.cab
-wget $URL/$ID/Data/$VERSION/i641033.cab
-wget $URL/$ID/Data/$VERSION/s320.cab
-wget $URL/$ID/Data/$VERSION/s640.cab
-wget $URL/$ID/Data/$VERSION/s321033.cab
-wget $URL/$ID/Data/$VERSION/s641033.cab
-wget $URL/$ID/Data/$VERSION/stream.x64.en-US.dat
-wget $URL/$ID/Data/$VERSION/stream.x64.en-US.dat.cobra
-wget $URL/$ID/Data/$VERSION/stream.x64.x-none.dat
-wget $URL/$ID/Data/$VERSION/stream.x64.x-none.dat.cobra
-wget $URL/$ID/Data/$VERSION/s641033.cab
+wget $URL/$ID/Office/Data/$VERSION/i320.cab
+wget $URL/$ID/Office/Data/$VERSION/i640.cab
+wget $URL/$ID/Office/Data/$VERSION/i321033.cab
+wget $URL/$ID/Office/Data/$VERSION/i641033.cab
+wget $URL/$ID/Office/Data/$VERSION/s320.cab
+wget $URL/$ID/Office/Data/$VERSION/s640.cab
+wget $URL/$ID/Office/Data/$VERSION/s321033.cab
+wget $URL/$ID/Office/Data/$VERSION/s641033.cab
+wget $URL/$ID/Office/Data/$VERSION/stream.x64.en-US.dat
+wget $URL/$ID/Office/Data/$VERSION/stream.x64.en-US.dat.cobra
+wget $URL/$ID/Office/Data/$VERSION/stream.x64.x-none.dat
+wget $URL/$ID/Office/Data/$VERSION/stream.x64.x-none.dat.cobra
+wget $URL/$ID/Office/Data/$VERSION/s641033.cab
