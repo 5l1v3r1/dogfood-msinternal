6e05244ac06c7f246ff80343a06a6d2ea12b6f16
Ronit Sajeey <rsajeey@hotmail.com>
2018-01-13 23:33:31 +0000

    Sort via file size for larger files get better downloading speed
    * Change to branch Dogfood_O16ProPlus-O15Visio_DevMain

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
-# Start of Microsoft Configuration
+# Start of Microsoft Office Configuration
 URL=officecdn.microsoft.com.edgesuite.net/pr
-ID=F3260CF1-A92C-4C75-B02E-D64C0A86A968
-VERSION=16.0.9001.2066
-# End of Microsoft Configuration
+ID=EA4A4090-DE26-49D7-93C1-91BFF9E53FC3 # Channel : Dogfood_O16ProPlus-O15Visio_DevMain
+VERSION=16.0.9009.1000 # You can grab the latest version number for branch 'Dogfood_O16ProPlus-O15Visio_DevMain' @ tools.thecollectionbook.info/latest/?guid=EA4A4090-DE26-49D7-93C1-91BFF9E53FC3
+# End of Microsoft Office Configuration
 
 # Creates the 'Office' directory, enters 'Office' directory & downloads all of the required files
 mkdir Office && cd Office
@@ -15,16 +15,15 @@ wget $URL/$ID/Office/Data/v64_$VERSION.cab
 
 # Creates the 'Office/Data/$VERSION' directory, enters the 'Office/Data/$VERSION' directory & downloads all of the required files
 mkdir $VERSION && cd $VERSION
-wget $URL/$ID/Office/Data/$VERSION/i320.cab
-wget $URL/$ID/Office/Data/$VERSION/i640.cab
-wget $URL/$ID/Office/Data/$VERSION/i321033.cab
-wget $URL/$ID/Office/Data/$VERSION/i641033.cab
-wget $URL/$ID/Office/Data/$VERSION/s320.cab
-wget $URL/$ID/Office/Data/$VERSION/s640.cab
-wget $URL/$ID/Office/Data/$VERSION/s321033.cab
-wget $URL/$ID/Office/Data/$VERSION/s641033.cab
-wget $URL/$ID/Office/Data/$VERSION/stream.x64.en-US.dat
-wget $URL/$ID/Office/Data/$VERSION/stream.x64.en-US.dat.cobra
 wget $URL/$ID/Office/Data/$VERSION/stream.x64.x-none.dat
-wget $URL/$ID/Office/Data/$VERSION/stream.x64.x-none.dat.cobra
+wget $URL/$ID/Office/Data/$VERSION/stream.x64.en-US.dat
+wget $URL/$ID/Office/Data/$VERSION/i640.cab
+wget $URL/$ID/Office/Data/$VERSION/i320.cab
+wget $URL/$ID/Office/Data/$VERSION/s640.cab
+wget $URL/$ID/Office/Data/$VERSION/s320.cab
+wget $URL/$ID/Office/Data/$VERSION/i641033.cab
+wget $URL/$ID/Office/Data/$VERSION/i321033.cab
 wget $URL/$ID/Office/Data/$VERSION/s641033.cab
+wget $URL/$ID/Office/Data/$VERSION/s321033.cab
+wget $URL/$ID/Office/Data/$VERSION/stream.x64.x-none.dat.cobra
+wget $URL/$ID/Office/Data/$VERSION/stream.x64.en-US.dat.cobra
