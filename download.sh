72b9a7819c5675fc80f2cea28b11e8c6ec2f2522
Ronit Sajeey <rsajeey@hotmail.com>
2018-01-15 21:40:48 +0000

    Change capitalisation to match Microsoft's version in regedit

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 # Start of Microsoft Office Configuration
 URL=officecdn.microsoft.com.edgesuite.net/pr
-ID=EA4A4090-DE26-49D7-93C1-91BFF9E53FC3 # Channel : Dogfood_O16ProPlus-O15Visio_DevMain
+ID=ea4a4090-de26-49d7-93c1-91bff9e53fc3 # Channel : Dogfood_O16ProPlus-O15Visio_DevMain
 VERSION=16.0.9009.1000 # You can grab the latest version number for branch 'Dogfood_O16ProPlus-O15Visio_DevMain' @ tools.thecollectionbook.info/latest/?guid=EA4A4090-DE26-49D7-93C1-91BFF9E53FC3
 # End of Microsoft Office Configuration
 
