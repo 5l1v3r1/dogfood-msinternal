c88d7cc8d12b1137cefe923d55c2693982db44bd
Ronit Sajeey <rsajeey@hotmail.com>
2018-01-16 22:28:33 +0000

    I have added this to the README

--- launch.bat
+++ launch.bat
@@ -1,2 +1,1 @@
-reg add HKLM\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v CDNBaseUrl /d http://officecdn.microsoft.com/pr/ea4a4090-de26-49d7-93c1-91bff9e53fc3
 setup.exe /configure configure.xml
