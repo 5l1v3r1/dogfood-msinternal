d490d2ac5114748b40c688299b145df181a095b8
Ronit Sajeey <rsajeey@hotmail.com>
2018-01-16 21:46:54 +0000

    Switch from uppercase characters to lowercase characters

--- launch.bat
+++ launch.bat
@@ -1,2 +1,2 @@
-REG ADD HKLM\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v CDNBaseUrl /d http://officecdn.microsoft.com/pr/ea4a4090-de26-49d7-93c1-91bff9e53fc3
+reg add HKLM\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v CDNBaseUrl /d http://officecdn.microsoft.com/pr/ea4a4090-de26-49d7-93c1-91bff9e53fc3
 setup.exe /configure configure.xml
