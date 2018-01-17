3d740e4fb8aad91081de55be16c8dd010097fd61
Ronit Sajeey <rsajeey@hotmail.com>
2018-01-17 23:05:23 +0000

    Download latest update from the internal development branch

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 # Start of Microsoft Office Configuration
 URL=officecdn.microsoft.com.edgesuite.net/pr
 ID=ea4a4090-de26-49d7-93c1-91bff9e53fc3 # Channel : Dogfood_O16ProPlus-O15Visio_DevMain
-VERSION=16.0.9009.1000 # You can grab the latest version number for branch 'Dogfood_O16ProPlus-O15Visio_DevMain' @ tools.thecollectionbook.info/latest/?guid=EA4A4090-DE26-49D7-93C1-91BFF9E53FC3
+VERSION=16.0.9011.1000 # You can grab the latest version number for branch 'Dogfood_O16ProPlus-O15Visio_DevMain' @ tools.thecollectionbook.info/latest/?guid=ea4a4090-de26-49d7-93c1-91bff9e53fc3
 # End of Microsoft Office Configuration
 
 # Downloads the file at the root directory
