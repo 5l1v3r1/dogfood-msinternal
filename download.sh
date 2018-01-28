df284defdaefc1437d1c0a2905c5a474c4ad64fe
Ronit Sajeey <rsajeey@hotmail.com>
2018-01-28 20:57:56 +0000

    Fixed issues when installing Office from another drive other than the C: drive

--- download.sh
+++ download.sh
@@ -12,7 +12,7 @@ VER=$(curl tools.thecollectionbook.info/latest/?guid=$ID)
 #
 
 echo 'reg add HKLM\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v CDNBaseUrl /d http://officecdn.microsoft.com/pr/'$ID > launch.bat
-echo 'cd %~dp0' >> launch.bat
+echo 'cd /d %~dp0' >> launch.bat
 echo 'cls' >> launch.bat
 echo 'setup.exe /configure configure.xml' >> launch.bat
 
