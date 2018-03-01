b9057bd324732eef246a34fc58fdc581856d43fb
Ronit Sajeey <rsajeey@hotmail.com>
2018-03-01 15:50:20 +0000

    Sync with Office16 repository

--- download.sh
+++ download.sh
@@ -1,56 +1,26 @@
 # Start of Microsoft Office Configuration
 URL=officecdn.microsoft.com.edgesuite.net/pr
-ID=ea4a4090-de26-49d7-93c1-91bff9e53fc3 # Channel : Dogfood_DevMain
+ID=ea4a4090-de26-49d7-93c1-91bff9e53fc3
 DIR=Office/Data
-VER=$(curl tools.thecollectionbook.info/latest/?guid=$ID)
 # End of Microsoft Office Configuration
 
-# Start of Initialising configuration files
+wget -q $URL/$ID/$DIR/MRO.cab && cabextract -qF MRO.xml MRO.cab && rm MRO.cab
+VER=$(grep 'Available' MRO.xml | awk -F">" '{print $2}' | awk -F"<" '{print $1}' && rm MRO.xml)
+
+# Creates the 'ClickToRun' directory, enters the 'ClickToRun' directory & downloads all of the required files
+mkdir ClickToRun && cd ClickToRun
+wget -q $URL/$ID/$DIR/$VER/i640.cab
+wget -q $URL/$ID/$DIR/$VER/i641033.cab
+
+# Extracts all the files from 'i640' & 'i6401033' cabinet (.cab) files
+cabextract -q *.cab && rm *.cab && cd ..
 
 #
 # launch.bat
 #
 
 echo '@echo off' > launch.bat
-echo 'reg add HKLM\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v CDNBaseUrl /d http://officecdn.microsoft.com/pr/'$ID' >nul' >> launch.bat
-echo 'cd /d %~dp0' >> launch.bat
-echo 'setup.exe /configure configure.xml' >> launch.bat
+echo 'OfficeClickToRun.exe deliverymechanism=ea4a4090-de26-49d7-93c1-91bff9e53fc3 platform=x64 culture=en-us productstoadd=ProPlusVolume_en-us_x-none baseurl=http://officecdn.microsoft.com/pr/ea4a4090-de26-49d7-93c1-91bff9e53fc3 ProPlusVolume.excludedapps=access,excel,groove,lync,onedrive,onenote,outlook,publisher updatesenabled=True cdnbaseurl=http://officecdn.microsoft.com/pr/ea4a4090-de26-49d7-93c1-91bff9e53fc3 version=16.0.9127.2004 mediatype=CDN' >> launch.bat
 
-#
-# configure.xml
-#
-
-echo '<Configuration>' > configure.xml
-echo '  <Add OfficeClientEdition="64">' >> configure.xml
-echo '    <Product ID="ProPlus2019Retail">' >> configure.xml
-echo '      <Language ID="en-us" />' >> configure.xml
-echo '      <ExcludeApp ID="Access" />' >> configure.xml
-echo '      <ExcludeApp ID="Excel" />' >> configure.xml
-echo '      <ExcludeApp ID="Groove" />' >> configure.xml
-echo '      <ExcludeApp ID="Lync" />' >> configure.xml
-echo '      <ExcludeApp ID="OneDrive" />' >> configure.xml
-echo '      <ExcludeApp ID="OneNote" />' >> configure.xml
-echo '      <ExcludeApp ID="Outlook" />' >> configure.xml
-echo '      <ExcludeApp ID="Publisher" />' >> configure.xml
-echo '    </Product>' >> configure.xml
-echo '  </Add>' >> configure.xml
-echo '</Configuration>' >> configure.xml
-
-# End of Initialising configuration files
-
-# Downloads the file at the root directory
-wget $URL/wsus/setup.exe
-
-# Creates the '$DIR' directory, enters the '$DIR' directory & downloads all of the required files
-mkdir -p $DIR && cd $DIR
-wget $URL/$ID/$DIR/v64.cab
-wget $URL/$ID/$DIR/v64_$VER.cab
-
-# Creates the '$DIR/$VER' directory, enters the '$DIR/$VER' directory & downloads all of the required files
-mkdir $VER && cd $VER
-wget $URL/$ID/$DIR/$VER/stream.x64.x-none.dat
-wget $URL/$ID/$DIR/$VER/stream.x64.en-us.dat
-wget $URL/$ID/$DIR/$VER/i640.cab
-wget $URL/$ID/$DIR/$VER/s640.cab
-wget $URL/$ID/$DIR/$VER/i641033.cab
-wget $URL/$ID/$DIR/$VER/s641033.cab
+# Packages all the downloaded contents into '$VER.zip'
+zip -9lr $VER.zip *
