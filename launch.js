787617c165e0845fb531af30aec728c05a5ca561
Ronit Sajeey <rsajeey@hotmail.com>
2018-11-28 23:24:54 +0000

    Version 3.0.6

--- launch.js
+++ launch.js
@@ -11,6 +11,6 @@ https.get('https://mrodevicemgr.edog.officeapps.live.com/mrodevicemgrsvc/api/v2/
         console.log();
         console.log('echo \'@echo off\' > launch.bat');
         console.log('echo \'xcopy %~dp0ClickToRun "%CommonProgramFiles%\\microsoft shared\\ClickToRun\\" >nul 2>&1\' >> launch.bat');
-        console.log('echo \'"%CommonProgramFiles%\\microsoft shared\\ClickToRun\\OfficeClickToRun.exe" deliverymechanism=' + obj[22].FFN + ' platform=x64 culture=en-us productstoadd=O365ProPlusRetail.16_en-us_x-none O365ProPlusRetail.excludedapps.16=groove,onenote updatesenabled.16=True cdnbaseurl.16=http://officecdn.microsoft.com/pr/' + obj[22].FFN + ' version.16=' + obj[22].AvailableBuild + ' mediatype.16=CDN baseurl.16=http://officecdn.microsoft.com/pr/' + obj[22].FFN + '\' >> launch.bat');
+        console.log('echo \'start "" "%CommonProgramFiles%\\microsoft shared\\ClickToRun\\OfficeClickToRun.exe" deliverymechanism=' + obj[22].FFN + ' platform=x64 culture=en-us productstoadd=O365ProPlusRetail.16_en-us_x-none O365ProPlusRetail.excludedapps.16=groove,onenote updatesenabled.16=True cdnbaseurl.16=http://officecdn.microsoft.com/pr/' + obj[22].FFN + ' version.16=' + obj[22].AvailableBuild + ' mediatype.16=CDN baseurl.16=http://officecdn.microsoft.com/pr/' + obj[22].FFN + '\' >> launch.bat');
     });
 });
