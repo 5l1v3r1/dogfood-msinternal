2ac6bdc91a18397500bc11a91ab2375905ba0bbe
Ronit Sajeey <rsajeey@hotmail.com>
2018-11-29 22:51:37 +0000

    Version 3.0.8

--- launch.js
+++ launch.js
@@ -3,14 +3,19 @@ const https = require('https');
 https.get('https://mrodevicemgr.edog.officeapps.live.com/mrodevicemgrsvc/api/v2/C2RReleaseData' + '?' + Math.floor(Date.now() / 1000), (res) => {
     res.on('data', (d) => {
         let obj = JSON.parse(d);
+
+        let url = 'http://officecdn.microsoft.com/pr';
+        let cdn = obj[22].FFN;
+        let ver = obj[22].AvailableBuild;
+
         console.log('mkdir ClickToRun');
-        console.log('wget -q http://officecdn.microsoft.com/pr/' + obj[22].FFN + '/Office/Data/' + obj[22].AvailableBuild + '/i640.cab -P ClickToRun');
-        console.log('wget -q http://officecdn.microsoft.com/pr/' + obj[22].FFN + '/Office/Data/' + obj[22].AvailableBuild + '/i641033.cab -P ClickToRun');
+        console.log('wget -q ' + url + '/' + cdn + '/Office/Data/' + ver + '/i640.cab -P ClickToRun');
+        console.log('wget -q ' + url + '/' + cdn + '/Office/Data/' + ver + '/i641033.cab -P ClickToRun');
         console.log('cabextract -q ClickToRun/*.cab -d ClickToRun');
         console.log('rm ClickToRun/*.cab');
         console.log();
         console.log('echo \'@echo off\' > launch.bat');
         console.log('echo \'xcopy "%~dp0"ClickToRun "%CommonProgramFiles%\\microsoft shared\\ClickToRun\\" >nul 2>&1\' >> launch.bat');
-        console.log('echo \'start "" "%CommonProgramFiles%\\microsoft shared\\ClickToRun\\OfficeClickToRun.exe" deliverymechanism=' + obj[22].FFN + ' platform=x64 culture=en-us productstoadd=O365ProPlusRetail.16_en-us_x-none O365ProPlusRetail.excludedapps.16=groove,onenote updatesenabled.16=True cdnbaseurl.16=http://officecdn.microsoft.com/pr/' + obj[22].FFN + ' version.16=' + obj[22].AvailableBuild + ' mediatype.16=CDN baseurl.16=http://officecdn.microsoft.com/pr/' + obj[22].FFN + '\' >> launch.bat');
+        console.log('echo \'start "" "%CommonProgramFiles%\\microsoft shared\\ClickToRun\\OfficeClickToRun.exe" deliverymechanism=' + cdn + ' platform=x64 culture=en-us productstoadd=O365ProPlusRetail.16_en-us_x-none O365ProPlusRetail.excludedapps.16=groove,onenote updatesenabled.16=True cdnbaseurl.16=' + url + '/' + cdn + ' version.16=' + ver + ' mediatype.16=CDN baseurl.16=' + url + '/' + cdn + '\' >> launch.bat');
     });
 });
