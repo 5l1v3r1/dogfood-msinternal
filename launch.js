d36d80c3606a7e8264fd0c86e171cbfa7d549c03
Ronit Sajeey <rsajeey@hotmail.com>
2018-12-22 18:15:03 +0000

    Version 3.0.9

--- launch.js
+++ launch.js
@@ -9,13 +9,13 @@ https.get('https://mrodevicemgr.edog.officeapps.live.com/mrodevicemgrsvc/api/v2/
         let ver = obj[22].AvailableBuild;
 
         console.log('mkdir ClickToRun');
-        console.log('wget -q ' + url + '/' + cdn + '/Office/Data/' + ver + '/i640.cab -P ClickToRun');
-        console.log('wget -q ' + url + '/' + cdn + '/Office/Data/' + ver + '/i641033.cab -P ClickToRun');
-        console.log('cabextract -q ClickToRun/*.cab -d ClickToRun');
+        console.log('wget ' + url + '/' + cdn + '/Office/Data/' + ver + '/i640.cab -P ClickToRun > /dev/null 2>&1');
+        console.log('wget ' + url + '/' + cdn + '/Office/Data/' + ver + '/i641033.cab -P ClickToRun > /dev/null 2>&1');
+        console.log('cabextract ClickToRun/*.cab -d ClickToRun > /dev/null 2>&1');
         console.log('rm ClickToRun/*.cab');
         console.log();
         console.log('echo \'@echo off\' > launch.bat');
-        console.log('echo \'xcopy "%~dp0"ClickToRun "%CommonProgramFiles%\\microsoft shared\\ClickToRun\\" >nul 2>&1\' >> launch.bat');
+        console.log('echo \'xcopy "%~dp0"ClickToRun "%CommonProgramFiles%\\microsoft shared\\ClickToRun\\" > nul 2>&1\' >> launch.bat');
         console.log('echo \'start "" "%CommonProgramFiles%\\microsoft shared\\ClickToRun\\OfficeClickToRun.exe" deliverymechanism=' + cdn + ' platform=x64 culture=en-us productstoadd=O365ProPlusRetail.16_en-us_x-none O365ProPlusRetail.excludedapps.16=groove,onenote updatesenabled.16=True cdnbaseurl.16=' + url + '/' + cdn + ' version.16=' + ver + ' mediatype.16=CDN baseurl.16=' + url + '/' + cdn + '\' >> launch.bat');
     });
 });
