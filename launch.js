e749cd7a0a3d1cd968838dda9dc2b07ca1848f1a
ItzLevvie <rsajeey@hotmail.com>
2019-05-07 16:08:16 +0100

    Version 4.0.5

--- launch.js
+++ launch.js
@@ -1,13 +1,13 @@
 const https = require('https');
 
-https.get('https://mrodevicemgr.officeapps.live.com/mrodevicemgrsvc/api/v2/C2RReleaseData?audienceFFN=ea4a4090-de26-49d7-93c1-91bff9e53fc3', (res) => {
+https.get('https://mrodevicemgr.edog.officeapps.live.com/mrodevicemgrsvc/api/v2/C2RReleaseData?audienceFFN=ea4a4090-de26-49d7-93c1-91bff9e53fc3', (res) => {
     res.on('data', (d) => {
         let obj = JSON.parse(d);
         let id = obj.FFN;
         let ver = obj.AvailableBuild;
 
         console.log(
-            `\
+`\
 mkdir ClickToRun
 
 wget http://officecdn.microsoft.com/pr/${id}/Office/Data/${ver}/i640.cab -P ClickToRun > /dev/null 2>&1
@@ -19,7 +19,7 @@ rm ClickToRun/i640.cab ClickToRun/i641033.cab
 echo '@echo off' > launch.bat
 echo 'robocopy %~dp0ClickToRun "%CommonProgramFiles%\\microsoft shared\\ClickToRun" > nul 2>&1' >> launch.bat
 echo 'start "" "%CommonProgramFiles%\\microsoft shared\\ClickToRun\\OfficeClickToRun.exe" deliverymechanism=${id} platform=x64 culture=en-us productstoadd=O365ProPlusRetail.16_en-us_x-none O365ProPlusRetail.excludedapps.16=groove,onenote updatesenabled.16=True cdnbaseurl.16=http://officecdn.microsoft.com/pr/${id} version.16=${ver} mediatype.16=CDN baseurl.16=http://officecdn.microsoft.com/pr/${id}' >> launch.bat
-            `
+`
         );
     });
 });
